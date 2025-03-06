194fa639cb48b9832eede6939d5a1849b6bec7d3
KristianColville1 <kristiancolville1@gmail.com>
2025-03-06 20:03:31 +0000

    Take ideas from real news websites

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/images");
 };
